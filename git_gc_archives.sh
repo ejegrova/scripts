185653ab68f2b3649f00c969750d0797733ec2be
ejegrova <ejegrova@redhat.com>
2020-12-21 16:51:40 +0100

    Remove echos

--- git_gc_archives.sh
+++ git_gc_archives.sh
@@ -1,25 +1,17 @@
-#cd /var/lib/cachito/sources
-cd var/lib/cachito/sources
-#find . -type f -size +10M
+cd /var/lib/cachito/sources
 
-my_array=( $(find . -type f) )
+my_array=( $(find . -type f -size +1G) )
 
 for element_path in "${my_array[@]}"
   do
-    echo "${element_path}"
     wc -c $element_path
     tar -zxf $element_path --one-top-level 
     element=$(echo $element_path | rev | cut -d '/' -f1 | rev | cut -d '.' -f1)
-    echo $element
-
-    sleep 5
     cd $element/app
-    pwd
-    #git gc
-
-    sleep 5
+    git gc
     cd ../..
     tar -czf $element_path $element
     wc -c $element_path
+    #sleep 5
   done
 
