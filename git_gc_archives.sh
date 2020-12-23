bc100728331cf0c1b6994cec5ed5833128c71dfe
ejegrova <ejegrova@redhat.com>
2020-12-23 16:26:03 +0100

    Modify git_gc_archives to use pushd/popd and /tmp/ dir

--- git_gc_archives.sh
+++ git_gc_archives.sh
@@ -1,18 +1,22 @@
-cd /var/lib/cachito/sources
+sources_dir="/var/lib/cachito/sources/"
+cd $sources_dir
 
 my_array=( $(find . -type f -size +1G) )
+dest_dir="/tmp/"
 
 for element_path in "${my_array[@]}"
   do
-    wc -c $element_path
-    tar -zxf $element_path --one-top-level 
-    element=$(echo $element_path | rev | cut -d '/' -f1 | rev | cut -d '.' -f1)
-    cd $element/app
+    echo "Before: $(wc -c $element_path)"
+    tar -zxf $element_path --one-top-level=$dest_dir
+    element=$(basename  $element_path .tar.gz)
+    pushd $dest_dir 1> /dev/null
+    pushd $dest_dir$element/app 1> /dev/null
     git gc --prune=now --quiet
-    cd ../..
-    tar -czf $element_path $element
-    wc -c $element_path
+    popd 1> /dev/null
+    tar -czf $sources_dir$element_path $element
     rm -rf ${element}
+    popd 1> /dev/null
+    echo "After: $(wc -c $element_path)"
     echo
     #sleep 5
   done
