d9ea7b91b88ce2985ad0c03084916af24226dac7
ejegrova <ejegrova@redhat.com>
2021-01-07 17:16:18 +0100

    Change paths for archive extraction/creation

--- git_gc_archives.sh
+++ git_gc_archives.sh
@@ -12,17 +12,20 @@ dest_dir="/tmp/"
 for element_path in "${my_array[@]}"
   do
     echo "Before: $(wc -c $element_path)"
-    tar -zxf $element_path --one-top-level=$dest_dir
     element=$(basename  $element_path .tar.gz)
+    element_dest_dir=$dest_dir$element
+    mkdir $element_dest_dir
+    tar -zxf $element_path -C $element_dest_dir 
     pushd $dest_dir 1> /dev/null
-    pushd $dest_dir$element/app 1> /dev/null
+    pushd $element_dest_dir 1> /dev/null
+    pushd ./app 1> /dev/null
     git gc --prune=now --quiet
     popd 1> /dev/null
-    tar -czf $sources_dir$element_path $element
+    tar -czf $sources_dir$element_path *
+    popd 1> /dev/null
     rm -rf ${element}
     popd 1> /dev/null
     echo "After: $(wc -c $element_path)"
     echo
-    #sleep 5
   done
 
